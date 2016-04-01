366aa140cd3ec751adccdbfe99ece112de91d831
AI-WebUI <ai_webui@163.com>
2016-04-01 10:19:56 +0800

    webui
    tess

---
+++ WEBUInew/html/mobile/extends/js/demopart.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2016/3/26.
+ */
+//$("body").append("<div></div>");
+//$("div:first-child").addClass("grid");
+//$(".grid").html("<div id='demo-grid'></div>").hide();
