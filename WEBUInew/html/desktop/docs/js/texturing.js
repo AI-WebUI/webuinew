e4ba8290acc0b775e92b4b23417f944110b9b288
AI-WebUI <ai_webui@163.com>
2016-04-14 13:23:04 +0800

    Update texturing.js

--- WEBUInew/html/desktop/docs/js/texturing.js
+++ WEBUInew/html/desktop/docs/js/texturing.js
@@ -2,7 +2,7 @@
  * Created by Administrator on 2016/4/6.
  */
 $(function () {
-    var html=$('body').html();
+    var html=$('html').html();
     $("pre").text(html).addClass("prettyprint linenums");
 //            $("#js").text($('script').text()).addClass("prettyprint linenums");
     prettyPrint();
